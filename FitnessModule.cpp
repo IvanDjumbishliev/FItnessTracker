88fa8454c70fb60bfecb4f89a5f4b2a047fa84ec
IvanDjumbishliev <ivan.n.dzhumbishliev.2023@elsys-bg.org>
2026-05-17 16:33:31 +0300

    implement User profile management

--- FitnessModule.cpp
+++ FitnessModule.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <random>
 #include <sstream>
+#include <stdexcept>
 
 namespace fitness
 {
@@ -40,6 +41,80 @@ namespace fitness
     const std::chrono::system_clock::time_point &BaseEntity::getCreatedAt() const noexcept
     {
         return createdAt;
+    }
+
+    User::User(std::string username,
+               std::string email,
+               double weightKg,
+               int heightCm,
+               GoalType goal)
+        : BaseEntity(),
+          username(std::move(username)),
+          email(std::move(email)),
+          weightKg(0.0),
+          heightCm(0),
+          goal(goal)
+    {
+        setWeightKg(weightKg);
+        setHeightCm(heightCm);
+    }
+
+    const std::string &User::getUsername() const noexcept
+    {
+        return username;
+    }
+
+    void User::setUsername(std::string username)
+    {
+        this->username = std::move(username);
+    }
+
+    const std::string &User::getEmail() const noexcept
+    {
+        return email;
+    }
+
+    void User::setEmail(std::string email)
+    {
+        this->email = std::move(email);
+    }
+
+    double User::getWeightKg() const noexcept
+    {
+        return weightKg;
+    }
+
+    void User::setWeightKg(double weightKg)
+    {
+        if (weightKg <= 0.0)
+        {
+            throw std::invalid_argument("weightKg must be greater than 0");
+        }
+        this->weightKg = weightKg;
+    }
+
+    int User::getHeightCm() const noexcept
+    {
+        return heightCm;
+    }
+
+    void User::setHeightCm(int heightCm)
+    {
+        if (heightCm <= 0)
+        {
+            throw std::invalid_argument("heightCm must be greater than 0");
+        }
+        this->heightCm = heightCm;
+    }
+
+    GoalType User::getGoal() const noexcept
+    {
+        return goal;
+    }
+
+    void User::setGoal(GoalType goal)
+    {
+        this->goal = goal;
     }
 
 } // namespace fitness
